7a1b31dbc55de9bac1e000940fb1f359f8971417
jiny-01 <allie7019@naver.com>
2026-01-19 09:30:47 +0900

    Main 대시보드 조회 Service 구현, 채Controller 구현중

---
+++ SsedamPet_back/src/main/java/com/korit/ssedampet_back/controller/MainController.java
@@ -0,0 +1,14 @@
+package com.korit.ssedampet_back.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Controller
+@RestController
+@RequestMapping("/api/main")
+public class MainController {
+
+
+}
