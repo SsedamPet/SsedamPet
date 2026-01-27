eae29fd6678a76fc1a271bb8a20d36f99dcc5335
jiny-01 <allie7019@naver.com>
2026-01-27 11:29:01 +0900

    메인페이지_펫 슬라이더_ã백엔드 petDto 에 petType 추가

--- SsedamPet_back/src/main/java/com/korit/ssedampet_back/dto/response/mypage/PetDto.java
+++ SsedamPet_back/src/main/java/com/korit/ssedampet_back/dto/response/mypage/PetDto.java
@@ -14,6 +14,7 @@ public class PetDto {
     private String petName;               // pet_tb.pet_name
     private String petGender;             // pet_tb.pet_gender ("M","F")
     private String petBreed;
+    private String petType;
     private String petProfileImgUrl;      // pet_tb.pet_profile_img_url
     private LocalDate petBirth;
     private int petAge;          // SQL에서 계산해서 담아줄 나이
