69ebd32e990e4072ca017e848b1e2994c0a1c009
jiny-01 <allie7019@naver.com>
2026-01-29 16:44:35 +0900

    경로 꼬여서 CommunityMain -> MainCommunity 변경

--- SsedamPet_front/src/pages/Community/MainCommunity/MainCommunity.jsx
+++ SsedamPet_front/src/pages/Community/MainCommunity/MainCommunity.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import CommentSection from "../Comment/CommentSection";
 
 function MainCommunity() {
   const navigate = useNavigate();
