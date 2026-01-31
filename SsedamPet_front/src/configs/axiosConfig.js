712a9b2f153b5e9b05c22de9a3a4fed3511d3f5c
jiny-01 <allie7019@naver.com>
2026-02-01 02:35:58 +0900

    .env.development, .env.production gitignore 에 추가, axiosConfig 수정

--- SsedamPet_front/src/configs/axiosConfig.js
+++ SsedamPet_front/src/configs/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "http://localhost:8080",
+  baseURL: import.meta.env.VITE_API_BASE_URL,
 });
 
 api.interceptors.request.use((config) => {
@@ -11,6 +11,15 @@ api.interceptors.request.use((config) => {
         config.headers.Authorization = `Bearer ${accessToken}`;
     }
 
-  // config.headers.Authorization = `Bearer ${accessToken}`;
   return config;
 });
+
+// 응답 인터셉터: 토큰 만료 및 예외처리
+api.interceptors.response.use((response) => response, (error) => {
+  if (error.response && error.response.status !== 200) {
+    alert("세션이 만료되었습니다. 다시 로그인해주세요");
+    localStorage.removeItem("AccessToken");
+    window.location.href = "/auth/login";
+  }
+  return Promise.reject(error);
+})
