03dfa2b3827b721c6ca9cde1510cbd92d7dcf5f1
Kyyy12345 <rladbsduf135@naver.com>
2026-02-02 11:11:16 +0900

    mypage.jsx 펫정보(이미지 빼고) 매핑 코드 수정

--- SsedamPet_front/src/react-query/queries/petsQueries.js
+++ SsedamPet_front/src/react-query/queries/petsQueries.js
@@ -1,13 +1,15 @@
 import { useQuery } from "@tanstack/react-query"
 import { getMyPets } from "../../apis/mypages/mypagesApi"
 
-export const useMyPetsQuery = () => {
-    useQuery({
+export const useMyPetsQuery = ( enabled = true ) => {
+    return useQuery({
         queryKey: ["myPets"],
+        enabled,
         queryFn: async () => {
             const res = await getMyPets();
             return res.data;
         },
+        
         retry: 0,
     });
 }
--- SsedamPet_front/src/react-query/queries/usersQueries.js
+++ SsedamPet_front/src/react-query/queries/usersQueries.js
@@ -1,18 +1,19 @@
 import { useQuery } from "@tanstack/react-query";
 import { requestMe } from "../../apis/users/usersApis";
 
-export const useMeQuery = () => {
+export const useMeQuery = (enabled = true) => {
   const accessToken = localStorage.getItem("AccessToken");
   // console.log("accessToken:", accessToken);
 
   return useQuery({
     queryKey: ["me", accessToken],
+    enabled: enabled && !!accessToken,
     queryFn: async () => {
       try {
         const res = await requestMe();
         console.log("requestMe success:", res);
         console.log("me data:", res.data);
-        return res;
+        return res.data;
       } catch (error) {
         console.log("requestMe error:", error);
         // return error.response;
